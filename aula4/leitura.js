f6ceedf59d0e8575a1100c9b3c5bc3c684a5fdc2
J3eff <jeffersonvideo125@gmail.com>
2023-11-02 12:21:37 -0300

    Operações com um JSON

--- aula4/leitura.js
+++ aula4/leitura.js
@@ -2,3 +2,13 @@ const dados = require("./cliente.json")
 
 console.log(dados)
 console.log(typeof dados)
+
+const clienteEmString = JSON.stringify(dados);
+
+console.log(clienteEmString);
+console.log(typeof clienteEmString);
+
+const cliente = JSON.parse(clienteEmString);
+
+console.log(cliente);
+
