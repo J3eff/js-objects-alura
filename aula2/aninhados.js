b61bf068281b9808f3a5bce9cbc84ace0729f50d
J3eff <jeffersonvideo125@gmail.com>
2023-11-02 00:41:07 -0300

    Objetos em objetos

---
+++ aula2/aninhados.js
@@ -0,0 +1,15 @@
+const cliente = {
+    nome: "João",
+    idade: 24,
+    email: "joao@gmail.com",
+    telefones: ["11911112222", "11933334444"],
+}
+
+cliente.endereco = {
+    rua: "R. Bento da silva",
+    numero: 1337,
+    apartamento: true,
+    complemento: "ap 71"
+};
+
+console.log(cliente.endereco);
